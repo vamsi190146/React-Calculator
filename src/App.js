1ae4a05897fa7f4fa80381c103e912306648d6c5
Tadiparthi Vamsi <145857734+vamsi190146@users.noreply.github.com>
2024-07-12 23:36:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,3 @@
-// import React, { useState } from 'react';
-// import Keypad from "./keypad"
-// import "./App.css"
-
-// function App(){
-
-//     let[input,setInput]=useState("")
-//     function handleClick(value){
-//         setInput(input+value)
-
-//     }
-//     function calculate(value){
-//         let outputval=eval(input)
-//         setInput(outputval)
-//     }
-//     function handleClear(){
-//         setInput("")
-//     }
-//   return (
-//     <div className="container">
-//       <h1>Calculator App </h1>
-//       <div className="calculator">
-//         <input type="text"  value ={input}className="output"/>
-//         <Keypad handleClick={handleClick} handleClear={handleClear} calculate={calculate}></Keypad>
-
-//       </div>
-
-//     </div>
-//   )
-
-// }
-// export default App
 
 import React, { useState } from 'react';
 import Keypad from "./keypad";
